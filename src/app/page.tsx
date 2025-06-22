c2cc4d827e9d706669afb5bc4e17ede942864c2d
Kas12344 <tsehar202@gmail.com>
2025-06-22 00:45:01 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,26 @@ import { getNewArrivals, getFeaturedLaptops, getPopularBrands } from '@/lib/data
 import type { Laptop, Brand } from '@/types';
 import { ChevronRight } from 'lucide-react';
 
+
+import { createClient } from '@/utils/supabase/server'
+import { cookies } from 'next/headers'
+export default async function Page() {
+  const cookieStore = await cookies()
+  const supabase = createClient(cookieStore)
+
+  const { data: todos } = await supabase.from('todos').select()
+
+  return (
+    <ul>
+      {todos?.map((todo) => (
+        <li>{todo}</li>
+      ))}
+    </ul>
+  )
+}
+
+
+
 export default async function HomePage() {
   const newArrivals: Laptop[] = await getNewArrivals(4);
   const featuredLaptops: Laptop[] = await getFeaturedLaptops(4);
