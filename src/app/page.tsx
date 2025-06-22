3bf3fc886d8972ce655b7508d61acc61fc0c9b8f
Kas12344 <tsehar202@gmail.com>
2025-06-22 00:54:03 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,23 +9,6 @@ import type { Laptop, Brand } from '@/types';
 import { ChevronRight } from 'lucide-react';
 
 
-import { createClient } from '@/utils/supabase/server'
-import { cookies } from 'next/headers'
-export default async function Page() {
-  const cookieStore = await cookies()
-  const supabase = createClient(cookieStore)
-
-  const { data: todos } = await supabase.from('todos').select()
-
-  return (
-    <ul>
-      {todos?.map((todo) => (
-        <li>{todo}</li>
-      ))}
-    </ul>
-  )
-}
-
 
 
 export default async function HomePage() {
